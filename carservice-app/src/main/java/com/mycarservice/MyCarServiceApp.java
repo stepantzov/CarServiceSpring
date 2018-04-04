b7ebc9f7e60e62335a592b2db4a292a567560feb
stepantzov <stepantzov@gmail.com>
2018-04-04 21:04:34 +0200

    CAR03 commit; -added spring bood parent to pom; -added up and running tomcat to the project root; -added spring annotation to the main method;

--- carservice-app/src/main/java/com/mycarservice/MyCarServiceApp.java
+++ carservice-app/src/main/java/com/mycarservice/MyCarServiceApp.java
@@ -1,10 +1,14 @@
 package com.mycarservice;
-
 import com.mycarservice.components.ComponentPrinter;
 import com.mycarservice.services.ServicePrinter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MyCarServiceApp {
+
     public static void main(String[] args) {
+        SpringApplication.run(MyCarServiceApp.class, args);
         ServicePrinter.printService();
         ComponentPrinter.printComponent();
     }
