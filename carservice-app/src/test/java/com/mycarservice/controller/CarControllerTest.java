11ce7684b98b81953b45a49b854522d9930d5f48
stepantzov <stepantzov@gmail.com>
2018-05-13 21:20:14 +0200

    CAR10 commit; -proceed with unit testing; -created unit test classes for service and controller;

--- carservice-app/src/test/java/com/mycarservice/controller/CarControllerTest.java
+++ carservice-app/src/test/java/com/mycarservice/controller/CarControllerTest.java
@@ -1,29 +1,94 @@
 package com.mycarservice.controller;
 
-import com.mycarservice.repository.CarRepository;
+import com.google.gson.Gson;
+import com.mycarservice.dto.CarDto;
+import com.mycarservice.dto.CarDtoTestStub;
+import com.mycarservice.service.CarService;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.assertEquals;
+import java.io.FileWriter;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 public class CarControllerTest {
 
-    @Autowired
-    private CarRepository carRepository;
+    CarDtoTestStub carDtoTestStub = new CarDtoTestStub();
+    CarDto carDto = carDtoTestStub.setTestCarDto();
 
-   /* @Before
-    public void preconditions() {
-        CarService carService = new CarService(carRepository);
-        CarDtoTestStub carDto = new CarDtoTestStub();
+    @Mock
+    private CarService carService;
 
-        carService.addCarInstance(carDto.setTestCarDto());
-    }*/
+    @InjectMocks
+    private CarController carController;
+
+    private MockMvc mockMvc;
+
+    /* @Autowired
+     private WebApplicationContext wac;
+    */
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(carController).build();
+        /*this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();*/
+        //TODO what is the difference in above?
+    }
 
     @Test
-    public void testCarController(){
-        CarController carController = new CarController();
-        System.out.println(carController.getCarEntityFromDatabase());
+    public void testAddCarAttributesHttpSuccessful() throws Exception {
+        //TODO: throws Exception?
+        Gson gson = new Gson();
 
-        assertEquals(carController.getCarEntityFromDatabase(), 1);
+        gson.toJson(carDto, new FileWriter("C:\\Maven\\CarService\\carservice-app\\file.json"));
+        String jsonCarDto = gson.toJson(carDto);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/car/insertCar").contentType(MediaType.APPLICATION_JSON).
+                content(jsonCarDto)).andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+
+       /* Mockito.verify(carService).addCarInstance(carDto); should this actually be included*/
+    }
+
+    @Test
+    public void testAddCarAttributesHttpFailed() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/car/insertCar").content("failed")).
+                andExpect(MockMvcResultMatchers.status().is4xxClientError());
+    }
+
+    @Test
+    public void testGetCarEntityFromDatabase() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/car/getStoredCarData")).
+                andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+
+    @Test
+    public void testGetExactCarEntityFromDatabase () throws Exception{
+    /*    Gson gson = new Gson();
+
+        gson.toJson(carDto, new FileWriter("C:\\Maven\\CarService\\carservice-app\\file.json"));
+        String jsonCarDto = gson.toJson(carDto);
+
+             this.mockMvc.perform(MockMvcRequestBuilders.post("/car/insertCar").contentType(MediaType.APPLICATION_JSON).
+                content(jsonCarDto)).andExpect(MockMvcResultMatchers.status().is2xxSuccessful());*/
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/car/getStoredCarData").accept(MediaType.APPLICATION_JSON)).
+                andExpect(MockMvcResultMatchers.status().is2xxSuccessful()).
+                andExpect(jsonPath("$.carId", Matchers.is(111)));
+
     }
 }
--- carservice-app/src/test/java/com/mycarservice/service/CarServiceTest.java
+++ carservice-app/src/test/java/com/mycarservice/service/CarServiceTest.java
@@ -1,6 +1,7 @@
 package com.mycarservice.service;
 
 import com.mycarservice.dto.CarDto;
+import com.mycarservice.dto.CarDtoTestStub;
 import com.mycarservice.entity.CarEntity;
 import com.mycarservice.repository.CarRepository;
 import org.junit.Assert;
@@ -16,7 +17,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.ArrayList;
 import java.util.List;
 
-//@ContextConfiguration
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 public class CarServiceTest {
 
@@ -27,20 +30,45 @@ public class CarServiceTest {
     private CarRepository carRepository;
 
     @Before
-    public void setup(){
+    public void setup() {
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void testGetCarDtosFromDatabase() {
-        List cars = new ArrayList();
+        List<CarEntity> cars = new ArrayList<>();
         cars.add(new CarEntity(1, "TestCarName", "TestCarDescription"));
-        Iterable <CarEntity> iterable = cars;
+        Iterable<CarEntity> iterable = cars;
         Mockito.when(carRepository.findAll()).thenReturn(iterable);
 
         List<CarDto> carDtos = carService.getCarDtosFromDatabase();
         Assert.assertEquals(carDtos.size(), 1);
         Assert.assertEquals(carDtos.get(0).getCarName(), "TestCarName");
         Assert.assertEquals(carDtos.get(0).getCarDescription(), "TestCarDescription");
+    }
+
+    @Test
+    public void testAddCarInstance() {
+        CarService carService = mock(CarService.class);
+        doNothing().when(carService).addCarInstance(isA(CarDto.class));
+        CarDtoTestStub carDtoTestStub = new CarDtoTestStub();
+        CarDto carDto = carDtoTestStub.setTestCarDto();
+
+        carService.addCarInstance(carDto);
+        verify(carService, times(1)).addCarInstance(carDto);
+    }
+
+    @Test(expected = Exception.class)
+    public void testAddCarInstanceThrowException() {
+        CarService carService = mock(CarService.class);
+        CarDtoTestStub carDtoTestStub = new CarDtoTestStub();
+        CarDto carDto = carDtoTestStub.setTestCarDto();
+        carService.addCarInstance(carDto);
+
+        doThrow(Exception.class).
+                when(carService).
+                addCarInstance(carDto);
+
+        verify(carService, times(1)).addCarInstance(carDto);
     }
 }
